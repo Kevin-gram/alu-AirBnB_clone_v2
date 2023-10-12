650dbda1cfcac3d79df6f420dfcdc494576de24b
kevin-gram <k.nyiringan@alustudent.com>
2023-10-12 15:47:39 +0300

    servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@
 from fabric.api import env, put, run
 from os.path import exists
 
-env.hosts = ["35.172.220.65", "34.204.53.150"]
+env.hosts = ["54.236.252.155", "23.20.7.32"]
 env.user = "ubuntu"
 env.key = "~/.ssh/id_rsa"
 
