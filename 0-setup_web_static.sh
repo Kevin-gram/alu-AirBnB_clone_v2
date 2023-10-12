b91f6b0d38d1b6794ba6d5b8318c123f1fd542b1
kevin-gram <k.nyiringan@alustudent.com>
2023-10-12 13:13:47 +0300

    updes on delpoyment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
-# Script that sets up your web servers for the deployment of web_static
 
-
+# Update packages and install Nginx
 sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo service nginx start
 
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+# Create directory structure
+sudo mkdir -p /data/web_static/{shared,releases/test}
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
+# Set permissions
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+# Configure Nginx
+sudo sed -i '/# pass the PHP/i location /hbnb_static {\n\talias /data/web_static/current/;\n}' /etc/nginx/sites-available/default
 
+# Restart Nginx
 sudo service nginx restart
+
