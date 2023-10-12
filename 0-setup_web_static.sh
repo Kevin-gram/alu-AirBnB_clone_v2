c3588b18ef5f35356a3e62ea7dbfe4a0e83a992c
kevin-gram <k.nyiringan@alustudent.com>
2023-10-12 14:03:00 +0300

    adding changes on deloyment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
+# Script that sets up your web servers for the deployment of web_static
 
-# Update packages and install Nginx
+
 sudo apt-get -y update
 sudo apt-get -y install nginx
+sudo service nginx start
 
-# Create directory structure
-sudo mkdir -p /data/web_static/{shared,releases/test}
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Set permissions
 sudo chown -R ubuntu:ubuntu /data/
 
-# Configure Nginx
-sudo sed -i '/# pass the PHP/i location /hbnb_static {\n\talias /data/web_static/current/;\n}' /etc/nginx/sites-available/default
+sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
-# Restart Nginx
 sudo service nginx restart
-
--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""
-Fabric script that generates a tgz archive from the contents of the web_static
-folder of the AirBnB Clone repo
-"""
+"""Fabric script that generates a .tgz archive from the contents of the"""
 
 
-from fabric.operations import local
+from fabric.api import local
 from datetime import datetime
+import os
 
 
 def do_pack():
-    """Function to compress files"""
-    local("mkdir -p versions")
-    result = local("tar -cvzf versions/web_static_{}.tgz web_static"
-                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
-                   capture=True)
-    if result.failed:
-        return None
-    return result
+    """Generates a .tgz archive from the contents
+    of the web_static folder"""
+    now = datetime.now()
+    now = now.strftime('%Y%m%d%H%M%S')
+    archive_path = 'versions/web_static_' + now + '.tgz'
+
+    local('mkdir -p versions/')
+    result = local('tar -cvzf {} web_static/'.format(archive_path))
+
+    if result.succeeded:
+        return archive_path
+    return None
